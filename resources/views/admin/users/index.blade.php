ce684f55169385a2b47604f19f77dd6f85b9b8c2
Tariq Bashir <onetariq@gmail.com>
2016-09-07 21:15:40 +1000

    fetched the users results in admin/users & fixed the layout issues working good now

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,48 @@
-<h1>This is working guys</h1>
+
+
+@extends('layouts.index')
+
+
+@section('sidebar')
+
+
+
+    <div class="row">
+        <div class="col-md-6">
+
+            @section('content')
+
+    <table class="table table-responsive">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Created</th>
+            <th>Updated</th>
+
+        </tr>
+        </thead>
+        <tbody>
+
+        @if($users)
+
+            @foreach($users as $user)
+                <tr>
+                    <td>{{$user->id}}</td>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->created_at->diffForHumans()}}</td>
+                    <td>{{$user->updated_at->diffForHumans()}}</td>
+                </tr>
+            @endforeach
+
+        @endif
+        @stop
+        </tbody>
+    </table>
+
+
+    </div>
+    </div>
+
